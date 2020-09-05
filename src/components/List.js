a5ab9b77306f90cdfc0d32536ed56928b58a5c07
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-09-05 21:50:19 +0530

    Update List component

--- src/components/List.js
+++ src/components/List.js
@@ -1,20 +1,23 @@
 import React from "react";
+import "styles/List.scss";
 
 const List = (props) => {
   const { repos } = props;
   if (!repos || repos.length === 0) return <h2>No repos, sorry.</h2>;
   return (
-    <ul>
+    <div>
       <h2>Available public repositories</h2>
-      {repos.map((repo) => {
-        return (
-          <li key={repo.id} className="list">
-            <span className="repo-text">{repo.name} </span>
-            <span className="repo-description">{repo.description}</span>
-          </li>
-        );
-      })}
-    </ul>
+      <ul>
+        {repos.map((repo) => {
+          return (
+            <li key={repo.id} className="list">
+              <a href={repo.html_url} className="repo-text">{repo.name} </a>
+              <span className="repo-description">{repo.description}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
