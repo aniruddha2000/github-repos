5e9fa5be76d5b3dc61fb4ce59081cae3ab19613b
Aniruddha Basak <codewithaniruddha@gmail.com>
2020-09-07 21:20:58 +0530

    Updates

---
+++ precache-manifest.1b11eacb93f221acf707f3c1421803f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb5394220ccfc78ad2b54ff571ccfd2",
+    "url": "/github-repos/index.html"
+  },
+  {
+    "revision": "464e456b48ff1ec1fcc4",
+    "url": "/github-repos/static/css/main.b74c52b6.chunk.css"
+  },
+  {
+    "revision": "b3b936cdbd9792659d3a",
+    "url": "/github-repos/static/js/2.74fbf946.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-repos/static/js/2.74fbf946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464e456b48ff1ec1fcc4",
+    "url": "/github-repos/static/js/main.2980da7f.chunk.js"
+  },
+  {
+    "revision": "1ea6ed0937997e09572d",
+    "url": "/github-repos/static/js/runtime-main.546e6590.js"
+  }
+]);
